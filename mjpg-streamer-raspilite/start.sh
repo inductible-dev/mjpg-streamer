5f9559d3f0b29c2b4825b16a066dee01c572f0dd
inductible.dev <inductible.dev@gmail.com>
2015-01-18 14:47:22 +0000

    trimmed out extraneous modules and code

---
+++ mjpg-streamer-raspilite/start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#/******************************************************************************
+#                                                                              #
+#      MJPG-streamer allows to stream JPG frames from an input-plugin          #
+#      to several output plugins                                               #
+#                                                                              #
+#      Copyright (C) 2007 Tom Stöveken                                         #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation; version 2 of the License.                      #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    #
+#                                                                              #
+#******************************************************************************/
+
+export LD_LIBRARY_PATH="$(pwd)"
+
+./mjpg_streamer -i "./input_raspicam.so -x 320 -y 240 -fps 25 -rot 270 -quality 10" -o "./output_http.so"
+#./mjpg_streamer -i "./input_uvc.so -d /dev/video0" -o "./output_http.so"
+
+
